bc4131e068fca74d149865e5935a8cb26f34d5ad
zulkifli kursani <zulkiflikursani@gmail.com>
2023-04-21 08:55:03 +0800

    update

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(
   cors({
     origin: "http://192.168.43.31:3000",
     origin: "https://frontend-zr.vercel.app",
+    origin: "https://frontend-zr.vercel.app/",
   })
 );
 app.use(bodyParser.json());
