ce9db36d586490e7ed23cdc813fde0213c1a9a29
zulkifli kursani <zulkiflikursani@gmail.com>
2023-05-15 23:33:47 +0800

    set cors

--- index.js
+++ index.js
@@ -82,10 +82,10 @@ app.post("/products-post", (req, res) => {
   try {
     con.query(sql, function (err, result) {
       if (err) throw err;
-      res.status(200).send(result);
+      res.status(200).json(result);
     });
   } catch (error) {
-    res.status(500).json({ msg: error.message + "error kiboss" });
+    res.status(500).json({ msg: error.message });
   }
 });
 // update produk
