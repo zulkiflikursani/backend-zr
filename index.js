47c5db4211ce3892a6dd168e7bdefc9412ded773
zulkifli kursani <zulkiflikursani@gmail.com>
2023-05-16 00:36:49 +0800

    set cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(function (req, res, next) {
   // Pass to next layer of middleware
   next();
 });
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.listen(port, hostname, () => {
