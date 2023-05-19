4007d5597cc7791de89fe2d180bde114cf10e4f6
zulkifli kursani <zulkiflikursani@gmail.com>
2023-05-19 23:38:56 +0800

    cors

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ var corsOptions = {
   credentials: true,
   origin: "http://localhost:3000",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(bodyParser.json());
