4825c8f916f894d4eab579e21cf51df5920ea4f0
zulkifli kursani <zulkiflikursani@gmail.com>
2023-05-19 23:41:51 +0800

    cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const con = require("./connection");
 const cors = require("cors");
 
