72c9a668522f73edb86b8aa8b8afb597c85e1c22
zulkifli kursani <zulkiflikursani@gmail.com>
2023-05-19 20:32:30 +0800

    cor

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(function (req, res, next) {
   next();
 });
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
