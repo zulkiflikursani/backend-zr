f781d5a44b4fc35e8ca2fbe37fe2c5a8f468839f
zulkifli kursani <zulkiflikursani@gmail.com>
2023-05-29 20:47:19 +0800

    patch update

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const http = require("http");
-const hostname =
-  process.env.HOSTNAME ||
-  "bv4yes5gbuhpqn8gsc3z-mysql.services.clever-cloud.com";
+
 const port = process.env.PORT || 3000;
 const app = require("./app");
 
