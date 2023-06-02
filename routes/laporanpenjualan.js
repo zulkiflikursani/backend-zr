a780af8d653e2d785909d1a8741823b7e3f8ce4a
zulkifli kursani <zulkiflikursani@gmail.com>
2023-06-02 22:09:16 +0800

    date format

--- routes/laporanpenjualan.js
+++ routes/laporanpenjualan.js
@@ -6,13 +6,13 @@ router.get("/", (req, res) => {
   const mulai = req.query.mulai;
   const sampai = req.query.sampai;
   const sql = `select penjualan.nama_barang,
-    penjualan.hjual,
-    FORMAT (penjualan.createAt, 'dd-MM-yy') as 'tanggal',
-    penjualan.kode_barang,
-    penjualan.kode_penjualan,
-    penjualan.qty,
-    product.hbeli,
-    penjualan.id from penjualan LEFT JOIN product on penjualan.kode_barang=product.id where  CAST(penjualan.createAt as date) BETWEEN '${mulai}' and '${sampai}'`;
+            penjualan.hjual,
+            DATE(penjualan.createAt) as tanggal,
+            penjualan.kode_barang,
+            penjualan.kode_penjualan,
+            penjualan.qty,
+            product.hbeli,
+            penjualan.id from penjualan LEFT JOIN product on penjualan.kode_barang = product.id where  CAST(penjualan.createAt as date) BETWEEN '${mulai}' and '${sampai}'`;
   // res.send(sql);
   con.query(sql, function (err, result) {
     if (err) throw err;
