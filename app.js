17f12698b41e1d36631e49c462b970bbd5d7342b
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2023-08-17 16:30:18 +0800

    missing cors

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
-    message: error.message,
+    message: "error: " + rerror.message,
   });
 });
 
