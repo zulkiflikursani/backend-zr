a19d72a05386ccb16daa44866673bd37974fa51a
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2023-08-26 15:25:53 +0800

    udpate penjualan

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const http = require("http");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+const hostname = process.env.HOSTNAME || "localhost";
 const app = require("./app");
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+// console(server);
+
 //s
