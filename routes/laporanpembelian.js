257266319992a74cc278e4df12481cb0be11e70a
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2023-08-26 15:55:55 +0800

    udpate penjualan

--- routes/laporanpembelian.js
+++ routes/laporanpembelian.js
@@ -7,7 +7,7 @@ router.get("/", (req, res) => {
   const mulai = req.query.mulai;
   const sampai = req.query.sampai;
   const sql =
-    "select pembelian.nama_barang,pembelian.hbeli,createAt as tangaal,pembelian.kode_barang,pembelian.kode_pembelian,pembelian.qty,pembelian.id from pembelian where CAST(pembelian.createAt as date) BETWEEN '${mulai}' and '${sampai}'";
+    "select pembelian.nama_barang,pembelian.hbeli,createAt as tangaal,pembelian.kode_barang,pembelian.kode_pembelian,pembelian.qty,pembelian.id from pembelian where CAST(pembelian.createAt as date) BETWEEN'2023-1-1' and '2023-12-12'";
   con.query(sql, function (err, result) {
     if (err) throw err;
     res.status(200).json(result);
