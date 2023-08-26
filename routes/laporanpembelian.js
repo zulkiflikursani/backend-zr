64bd037f400d05ab8217f06838e3e11452fc6346
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2023-08-26 14:08:54 +0800

    laporan Penjualan

---
+++ routes/laporanpembelian.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const con = require("../connection");
+const { route } = require("./Pembelian");
+
+router.get("/", (req, res) => {
+  const mulai = req.query.mulai;
+  const sampai = req.query.sampai;
+  const sql =
+    "select pembelian.nama_barang,pembelian,hbeli,createAt as tangaal,pembelian.kode_barang,pembelian.kode_pembelian,pembelian.qty,pembelian.id form pembelian where CAST(pembelian.createAt as date) BETWEEN '${mulai}' and '${sampai}'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.status(200).json(result);
+  });
+});
+
+module.exports = router;
