e9c4136dd7fcca453ed721cc1f9c1e1332e435b1
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2023-08-26 18:17:38 +0800

    udpate penjualan

--- routes/laporanpembelian.js
+++ routes/laporanpembelian.js
@@ -5,8 +5,7 @@ const con = require("../connection");
 router.get("/", (req, res) => {
   const mulai = req.query.mulai;
   const sampai = req.query.sampai;
-  const sql =
-    "select pembelian.nama_barang,pembelian.hbeli,createAt as tangaal,pembelian.kode_barang,pembelian.kode_pembelian,pembelian.qty,pembelian.id from pembelian where CAST(pembelian.createAt as date) BETWEEN '${mulai}' and '${sampai}'";
+  const sql = `select pembelian.nama_barang,pembelian.hbeli,createAt as tangaal,pembelian.kode_barang,pembelian.kode_pembelian,pembelian.qty,pembelian.id from pembelian where CAST(pembelian.createAt as date) BETWEEN '${mulai}' and '${sampai}'`;
   con.query(sql, function (err, result) {
     if (err) throw err;
     res.status(200).json(result);
