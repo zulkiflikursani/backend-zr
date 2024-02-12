08dd8f75a06849ea1f514bbb14ef5b1998472578
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2024-02-12 14:56:06 +0800

    origin master

--- app.js
+++ app.js
@@ -13,39 +13,27 @@ import cors from "cors";
 const app = express.Router();
 
 app.use(morgan("dev"));
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    message: "error:" + error.message,
-  });
-});
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    message: "error:" + error.message,
-  });
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+
   res.header(
     "Access-Control-Allow-Headers",
+
     "Origin, X-Requseted-With, Content-Type, Accept, Authorization"
   );
+
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
+
     return res.status(200).json({});
   }
+
   next();
 });
-// const corsoption = {
-//   origin: ["https://frontend-zr.vercel.app", "http://localhost:3000"],
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   credentials: true,
-// };
 
-// app.use(cors(corsoption));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/token", token);
@@ -54,5 +42,21 @@ app.use("/products", product);
 app.use("/pembelian", pembelian);
 app.use("/penjualan", penjualan);
 app.use("/laporanpenjualan", laporanPenjualan);
+
+app.use((req, res, next) => {
+  const error = new Error("not found");
+
+  error.status = 404;
+
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+
+  res.json({
+    message: "error:" + error.message,
+  });
+});
 
 export default app;
