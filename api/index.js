6c797440427550b3d65f9dcf604fa55d0130c149
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2024-02-12 18:37:46 +0800

    testing .com

--- api/index.js
+++ api/index.js
@@ -1,14 +1,1 @@
-// var createError = require("http-errors");
-// import createError from "http-error";
-import express from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-// var path = require("path");
-// var cookieParser = require("cookie-parser");
-// var logger = require("morgan");
-
 import "../server.js";
-import App from "../app.js";
-
-export default App;
