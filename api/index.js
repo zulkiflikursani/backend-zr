a974f2cf8d530380a44e62620654dac540270b13
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2024-02-12 18:09:18 +0800

    ad

--- api/index.js
+++ api/index.js
@@ -11,41 +11,4 @@ import logger from "morgan";
 import "../server.js";
 import App from "../app.js";
 
-var app = express();
-
-const whitelist = ["*"];
-
-app.use((req, res, next) => {
-  const origin = req.get("referer");
-  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-  if (isWhitelisted) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,Content-Type,Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-  }
-  // Pass to next layer of middleware
-  if (req.method === "OPTIONS") res.sendStatus(200);
-  else next();
-});
-
-const setContext = (req, res, next) => {
-  if (!req.context) req.context = {};
-  next();
-};
-app.use(setContext);
-
-app.use("/", App);
-app.get("/api", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: tes`);
-});
-
-export default app;
+export default App;
--- server.js
+++ server.js
@@ -5,6 +5,40 @@ import dotenv from "dotenv";
 dotenv.config();
 const port = 5000;
 const apps = express();
+const whitelist = ["*"];
+
+app.use((req, res, next) => {
+  const origin = req.get("referer");
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,Content-Type,Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === "OPTIONS") res.sendStatus(200);
+  else next();
+});
+
+const setContext = (req, res, next) => {
+  if (!req.context) req.context = {};
+  next();
+};
+app.use(setContext);
+
+app.use("/", app);
+app.get("/api", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: tes`);
+});
 
 try {
   await con.authenticate();
