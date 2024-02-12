f57714d5b7e3255b98d0be5a32d1b0db9d8e2873
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2024-02-12 15:58:57 +0800

    test

--- api/index.js
+++ api/index.js
@@ -42,5 +42,11 @@ const setContext = (req, res, next) => {
 app.use(setContext);
 
 app.use("/", App);
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
 export default app;
